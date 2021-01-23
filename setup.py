7fef2c432af207293d241e39986252d5a6f7f2cf
Tomaz Solc <tomaz.solc@tablix.org>
2021-01-23 12:48:28 +0100

    Drop support for ancient jsonschema <= 2.4.0
    jsonschema 2.4.0 was released in Aug 2014.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='jsonmerge',
     author='Tomaz Solc',
     author_email='tomaz.solc@tablix.org',
     packages = [ 'jsonmerge' ],
-    install_requires = [ 'jsonschema' ],
+    install_requires = [ 'jsonschema>2.4.0' ],
     test_suite = 'tests',
     classifiers = [
         "License :: OSI Approved :: MIT License",
