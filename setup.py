fdaa19c11e04a74255ce6e62f91c465d727d4065
Tomaz Solc <tomaz.solc@tablix.org>
2021-01-23 12:29:41 +0100

    Set version to 1.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='jsonmerge',
-    version='1.7.0',
+    version='1.8.0',
     description='Merge a series of JSON documents.',
     license='MIT',
     long_description=open("README.rst").read(),
